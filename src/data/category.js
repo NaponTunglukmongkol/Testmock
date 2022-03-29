cc5bcac6d5752acf8f4af33f6fc493d6abc09b98
Napon Tunglukmongkol <60070037@kmitl.ac.th>
2022-03-29 11:22:29 +0700

    Home page now generate categories from data

---
+++ src/data/category.js
@@ -0,0 +1,34 @@
+let category = [
+    {
+        id: 0,
+        title: 'เพลย์ลิสต์แนะนำสำหรับคุณ'
+    },
+    {
+        id: 1,
+        title: 'Charts'
+    },
+    {
+        id: 2,
+        title: 'Focus'
+    },
+    {
+        id: 3,
+        title: 'Mood'
+    },
+    {
+        id: 4,
+        title: 'Popular new releases'
+    },
+    {
+        id: 5,
+        title: 'ให้เสียงเพลงอยู่เป็นเพื่อนคุณ'
+    },
+    {
+        id: 6,
+        title: 'Soundtrack your home'
+    },
+]
+
+export function getCategory(){
+    return category;
+}
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,55 +1,29 @@
 import '../css/font.css';
 import '../css/home.css';
 import Card from '../component/Card.js';
+import { getCategory } from '../data/category.js';
 
 function Home() {
+  let data = getCategory();
+
   return (
     <div className="grey display fullscreen content">
-      <div className='grey fullscreen'>
-        <h4 className='title'>เพลย์ลิสต์แนะนำสำหรับคุณ</h4>
-        <div className='d-flex card-placement'>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
-        <h4 className='title'>เพลย์ลิสต์แนะนำสำหรับคุณ</h4>
-        <div className='d-flex card-placement'>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
-        <h4 className='title'>เพลย์ลิสต์แนะนำสำหรับคุณ</h4>
-        <div className='d-flex card-placement'>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
-        <h4 className='title'>เพลย์ลิสต์แนะนำสำหรับคุณ</h4>
-        <div className='d-flex card-placement'>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
+      <div className='grey fullscreen test'>
+        {data.map((category) => (
+          <div className='pad'>
+            <h4 className='title'>{category.title}</h4>
+            <div className='d-flex card-placement'>
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
